54e31b9baec08ac387e6fe4c641edf6db852bfb5
YI <109718292+KL-VC@users.noreply.github.com>
2023-08-05 17:40:28 +0800

    Update __init__.py

--- BGRL/bgrl/__init__.py
+++ BGRL/bgrl/__init__.py
@@ -4,4 +4,4 @@ from .scheduler import CosineDecayScheduler
 from .models import GCN, GraphSAGE_GCN
 from .transforms import get_graph_drop_transform
 from .utils import set_random_seeds
-from .logistic_regression_eval import fit_logistic_regression,fit_logistic_regression_fixed_split
+from .logistic_regression_eval import fit_logistic_regression
